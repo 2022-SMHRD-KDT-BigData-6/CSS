160a21f1cd253a565752cf4b2e6578fd381ead8c
GITCT <GITCT@DESKTOP-MLKIHA8>
2022-08-08 09:38:22 +0900

    수어단어 이미지 업로드 - 김은영

--- CSS_SpringMVC/src/main/java/kr/smhrd/mapper/mysql.sql
+++ CSS_SpringMVC/src/main/java/kr/smhrd/mapper/mysql.sql
@@ -4,3 +4,4 @@ select * from sl;
 select * from used_sl;
 
 
+insert into used_sl(sl_index, sl_word) values(1, '왼쪽');
---
+++ CSS_SpringMVC/src/main/java/kr/smhrd/model/FreqVO.java
@@ -0,0 +1,12 @@
+package kr.smhrd.model;
+
+import lombok.Data;
+
+@Data
+public class FreqVO {
+	private int idx;
+	private int sl_index;
+	private String sl_word;
+	private String sdate;
+
+}
